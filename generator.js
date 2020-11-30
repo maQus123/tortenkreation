68e0271806a96d87c6ec5afef3393834287fd846
Markus <markus@luehrmann.net>
2020-11-30 23:54:16 +0100

    goes on

--- generator.js
+++ generator.js
@@ -4,7 +4,7 @@ const Contentful = require("contentful");
 const Dotenv = require('dotenv');
 const Fetch = require('node-fetch');
 
-function initFolders() {
+function initDist() {
     Filesystem.rmdirSync('dist', { recursive: true });
     Filesystem.mkdirSync('dist');
     Filesystem.mkdirSync('dist/torten');
@@ -28,53 +28,44 @@ function getData() {
     });
 }
 
-function downloadImages(data) {
-    // TODO: Download one thumb image for each cake
-    data.items.forEach(item => {
+function downloadImages(items) {
+    items.forEach(item => {
+        downloadImage(
+            `https:${item.fields.images[0].fields.file.url}?fm=jpg&q=75&fit=thumb&w=500&h=375`,
+            `./dist/img/${item.fields.slug}-thumb.jpg`);
         item.fields.images.forEach((image, i) => {
-            Fetch(`https:${image.fields.file.url}?fm=jpg&q=75&fit=thumb&w=500&h=375`)
-                .then(res => {
-                    const path = `./dist/img/${item.fields.slug}-${i + 1}.jpg`;
-                    const stream = Filesystem.createWriteStream(path);
-                    res.body.pipe(stream);
-                });
+            downloadImage(
+                `https:${image.fields.file.url}?fm=jpg&q=90&w=1500&h=1125`,
+                `./dist/img/${item.fields.slug}-${i}.jpg`);
         });
     });
 }
 
+function downloadImage(sourceUrl, destinationPath) {
+    Fetch(sourceUrl).then(response => {
+        const stream = Filesystem.createWriteStream(destinationPath);
+        response.body.pipe(stream);
+    });
+}
+
+function generateHtml(sourcePath, destinationPath, data) {
+    let source = Filesystem.readFileSync(sourcePath, 'utf8').toString();
+    let template = Handlebars.compile(source);
+    let html = template(data);
+    Filesystem.writeFile(destinationPath, html, function (err) {
+        if (err) return console.log(err);
+    });
+}
+
 (function () {
-    initFolders();
+    initDist();
     Dotenv.config();
     getData().then(data => {
-
-        downloadImages(data);
-
-        //index.html
-        const indexPage = Filesystem.readFileSync('./src/index.html', 'utf8').toString();
-        Filesystem.writeFile('./dist/index.html', indexPage, function (err) {
-            if (err) return console.log(err);
-        });
-
-        //torten.html
-        const cakesTemplate = Handlebars.compile(Filesystem.readFileSync('./src/torten.html', 'utf8').toString());
-        const cakesHtml = cakesTemplate(data);
-        Filesystem.writeFile('./dist/torten.html', cakesHtml, function (err) {
-            if (err) return console.log(err);
-            console.log('Success torten.html');
-        });
-
-        //torte.html
-        const cakeTemplate = Handlebars.compile(Filesystem.readFileSync('./src/torte.html', 'utf8').toString());
+        downloadImages(data.items);
+        generateHtml('./src/torten.html', './dist/torten.html', data);
+        generateHtml('./src/index.html', './dist/index.html', data);
         data.items.forEach(item => {
-            const slug = item.fields.slug;
-            const cakeHtml = cakeTemplate(item);
-            Filesystem.writeFile(`./dist/torten/${slug}.html`, cakeHtml, function (err) {
-                if (err) return console.log(err);
-                console.log(`Success ${slug}.html`);
-            });
+            generateHtml('./src/torte.html', `./dist/torten/${item.fields.slug}.html`, item);
         });
-
     });
 })();
-
-// TODO: Refactor and clean
